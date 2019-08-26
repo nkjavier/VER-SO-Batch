a486ce2e10943d109f4e543c47e9f9523810cd73
nkjavier <54413139+nkjavier@users.noreply.github.com>
2019-08-26 14:41:18 -0300

    carga del archivo

---
+++ verSO-pc.bat
@@ -0,0 +1,20 @@
+@echo off
+echo.
+wmic os get caption
+wmic os get osarchitecture
+rem otra opcion
+rem systeminfo | findstr /B /C:"OS Name" /C:"OS Version"
+echo.
+rem otra opcion
+rem Reg query "\\%lista%\HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName | find /i "REG_SZ"
+echo Otra Opcion
+echo.
+echo.
+echo.
+ver | find /i "5.1" >NUL  2>NUL
+if %errorlevel% equ 0 echo Posee windows xp
+ver | find /i "6" >NUL  2>NUL
+if %errorlevel% equ 0 echo Posee windows 7
+ver | find /i "10" >NUL  2>NUL
+if %errorlevel% equ 0 echo Posee windows 10
+
